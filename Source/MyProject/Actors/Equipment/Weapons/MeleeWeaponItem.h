bf5413cf9fb63d911731d44f9b04cb3eb7a62c60
olegz <oleg.zylevich@hf-solution.com>
2025-02-18 19:30:48 +0300

    update project with new mechenics

---
+++ Source/MyProject/Actors/Equipment/Weapons/MeleeWeaponItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Equipment/EquipableItem.h"
+#include "MeleeWeaponItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API AMeleeWeaponItem : public AEquipableItem
+{
+	GENERATED_BODY()
+	
+};
