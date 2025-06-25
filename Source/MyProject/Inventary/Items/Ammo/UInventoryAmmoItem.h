fe2568cc18016a7a49465cdd54a394d04e66a3cc
olegz <oleg.zylevich@hf-solution.com>
2025-06-25 15:32:48 +0300

    Add UInvenotyAmmoItem

---
+++ Source/MyProject/Inventary/Items/Ammo/UInventoryAmmoItem.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Inventary/InventoryItem.h"
+#include "UInventoryAmmoItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UUInventoryAmmoItem : public UInventoryItem
+{
+	GENERATED_BODY()
+};
