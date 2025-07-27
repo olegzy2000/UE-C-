1c218b03823d887cd48bb96c1ffad4aed4c1b5a3
olegz <oleg.zylevich@hf-solution.com>
2025-07-27 23:01:59 +0300

    started develop saving system

---
+++ Source/MyProject/Subsystems/SaveSubsystem/SaveData.cpp
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SaveData.h"
+FBaseSaveData::FBaseSaveData() {
+
+}
+bool FBaseSaveData::Serialize(FArchive& Archive) {
+	Archive << Name;
+	return true;
+}
+FObjectSaveData::FObjectSaveData() {
+
+}
+FObjectSaveData::FObjectSaveData(const UObject* Object) :Class(nullptr) {
+	if (IsValid(Object)) {
+		Name = FName(Object->GetName());
+		Class = TStrongObjectPtr<UClass>(Object->GetClass());
+	}
+}
+
+bool FObjectSaveData::Serialize(FArchive& Archive)
+{
+	Super::Serialize(Archive);
+	UClass* ClassPtr = Class.Get();
+	Archive << ClassPtr;
+	if (Archive.IsLoading() && IsValid(ClassPtr)) {
+		Class = TStrongObjectPtr<UClass>(ClassPtr);
+	}
+	Archive << RawData;
+	return true;
+}
+
+FActorSaveData::FActorSaveData(const AActor* Actor)
+	:Super(Actor)
+	,Transform(FTransform::Identity)
+{
+
+}
+
+bool FActorSaveData::Serialize(FArchive& Archive)
+{
+	Super::Serialize(Archive);
+	Archive << Transform;
+	return true;
+}
+FActorSaveData::FActorSaveData()
+	:Transform(FTransform::Identity)
+{
+
+}
+
+FLevelSaveData::FLevelSaveData(const FName& LevelName)
+{
+	Name = LevelName;
+}
+
+bool FLevelSaveData::Serialize(FArchive& Archive)
+{
+	Super::Serialize(Archive);
+	Archive << ActorsSaveData;
+	return true;
+}
+
+bool FGameSaveData::Serialize(FArchive& Archive)
+{
+	Super::Serialize(Archive);
+	Archive << LevelName;
+	Archive << Level;
+	Archive << GameInstance;
+	Archive << StartTransform;
+	return true;
+}
+FGameSaveData::FGameSaveData()
+	: Level(FName(TEXT("Persistent")))
+	, StartTransform(FTransform::Identity)
+{
+
+}
---
+++ Source/MyProject/Subsystems/SaveSubsystem/SaveData.h
@@ -0,0 +1,84 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/StrongObjectPtr.h"
+#include "SaveData.generated.h"
+/**
+ * 
+ */
+
+USTRUCT()
+struct FBaseSaveData 
+{
+
+	GENERATED_BODY()
+
+public:
+	FBaseSaveData();
+	virtual ~FBaseSaveData(){}
+	virtual bool Serialize(FArchive& Archive);
+	friend FArchive& operator << (FArchive& Archive, FBaseSaveData& SaveData) 
+	{
+		SaveData.Serialize(Archive);
+		return Archive;
+	}
+	FName Name;
+};
+FORCEINLINE bool operator == (const FBaseSaveData& First, const FBaseSaveData& Second) { return First.Name == Second.Name; };
+
+USTRUCT()
+struct FObjectSaveData : public FBaseSaveData
+{
+
+	GENERATED_BODY()
+
+public:
+	FObjectSaveData();
+	FObjectSaveData(const UObject* Object);
+	virtual bool Serialize(FArchive& Archive) override;
+	TStrongObjectPtr<UClass> Class;
+	TArray<uint8> RawData;
+};
+
+USTRUCT()
+struct FActorSaveData : public FObjectSaveData
+{
+
+	GENERATED_BODY()
+
+public:
+	FActorSaveData();
+	FActorSaveData(const AActor* Object);
+	virtual bool Serialize(FArchive& Archive) override;
+	FTransform Transform;
+	TArray<FObjectSaveData> ComponentsSaveData;
+};
+
+USTRUCT()
+struct FLevelSaveData : public FBaseSaveData
+{
+
+	GENERATED_BODY()
+
+public:
+	FLevelSaveData();
+	FLevelSaveData(const FName& LevelName);
+	virtual bool Serialize(FArchive& Archive) override;
+	TArray<FActorSaveData> ActorsSaveData;
+};
+USTRUCT()
+struct FGameSaveData : public FBaseSaveData
+{
+
+	GENERATED_BODY()
+
+public:
+	FGameSaveData();
+	virtual bool Serialize(FArchive& Archive) override;
+	FName LevelName;
+	FLevelSaveData Level;
+	FObjectSaveData GameInstance;
+	FTransform StartTransform;
+};
